328352abaa8e49992dd312332c7f58e5d1716c7d
t.pickett66 <t.pickett66@gmail.com>
2012-12-12 19:00:09 -0600

    break a long line
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

--- lib/code_example/step_0.rb
+++ lib/code_example/step_0.rb
@@ -5,7 +5,8 @@ class EDIDigesterFactory
   def find_isa_segments(file_contents)
     @isa_indices = [0]
     delimeter = file_contents[105,1]
-    regex = /[#{delimeter}]?ISA([\*\|])0[0|3]\1.{10}\1[0][0|1]\1.{10}\1.{2}\1.{15}\1.{2}\1.{15}\1\d{6}\1\d{4}\1.\1\d{5}\1.{9}\1[0|1]\1[P|T]\1.#{delimeter}/
+    regex = /[#{delimeter}]?ISA([\*\|])0[0|3]\1.{10}\1[0][0|1]\1.{10}\1.{2}\1.{15}
+    \1.{2}\1.{15}\1\d{6}\1\d{4}\1.\1\d{5}\1.{9}\1[0|1]\1[P|T]\1.#{delimeter}/x
     until @isa_indices.last.nil?
       found_index = file_contents.index(regex,@isa_indices.last + 105)
       found_index += 1 if !found_index.nil? && file_contents[found_index,1] != "I"

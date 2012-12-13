bda6f334c444bedb76b3b8dbf7495fca1c965558
t.pickett66 <t.pickett66@gmail.com>
2012-12-12 19:43:25 -0600

    add first step of refactoring
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

---
+++ lib/code_example/step_1.rb
@@ -0,0 +1,19 @@
+class EDIDigesterFactory
+  def find_isa_segments(file_contents)
+    return @_isa_indices unless @_isa_indices.nil?
+    identifier =  ISASegmentIdentifier.new(file_contents)
+    @_isa_indices = identifier.find_indices
+  end
+end
+
+module ISASegmentIdentifier
+  attr_reader :file_contents
+
+  def initialize(file_contents)
+    @file_contents = file_contents
+  end
+
+  def find_indices
+    # big nasty method copied here...
+  end
+end
